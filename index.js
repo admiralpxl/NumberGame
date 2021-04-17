132bb5e8437be0b466340656693f891342b0f2ff
admiralpxl <wnstnjr@gmail.com>
2021-04-16 21:53:46 -0400

    Secuencia en Aleatorio sin repetirse los numeros

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PUNTOS = document.getElementById("puntos");
 const NIVEL = document.getElementById("nivel");
 const TIEMPO = document.getElementById("tiempo");
 const parrafo = document.getElementById("par");
+var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 //Una forma de insertar texto en html desde javascript //parrafo.innerHTML = "Texto Ejemplo"
 //Otra forma de inserta texto en html desde javascript //parrafo.textContent = "texto Ejemplo"
@@ -11,11 +12,32 @@ const parrafo = document.getElementById("par");
 PUNTOS.innerHTML = 1
 NIVEL.innerHTML = 2
 TIEMPO.innerHTML = 3
-
+/*
 for (let i = 0; i< a.length; i++) {
         a[i].innerHTML = i + 1
-    }
+    }*/
 
+    //const Aleatorio = () => Math.floor(Math.random() * 9 + 1 )
+    const Escribir = (lugar, texto) => lugar.innerHTML = texto
+
+   // Escribir(a[1], "Hola")
+
+   
+    
+    //numeros = numeros.sort( () => Math.random() -0.5) 
+   
+    function revolver(num) {
+      num.sort( () => Math.random() -0.5)
+   } 
+ 
+    function asignar(){
+
+       for (let i = 0; i < a.length; i++) {
+       console.log(`Numeros ${numeros[i]}`)
+       Escribir(a[i], numeros[i])
+         }
+       }
+    
 
 
 function esconder(){
@@ -28,6 +50,8 @@ function esconder(){
 
 function iniciar(){
     esconder();
+    revolver(numeros)
+    asignar()
 }
 
 
